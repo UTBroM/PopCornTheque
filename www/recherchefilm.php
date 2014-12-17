3eb8f1dc6a300374fbae28a51cd09f9700ff7f3a
Paul TREHIOU <paul.trehiou@utbm.fr>
2014-12-17 09:41:07 +0100

    Correction bug timeout en sauvegardant

--- www/recherchefilm.php
+++ www/recherchefilm.php
@@ -3,6 +3,10 @@
 
 <?php
 
+        echo ini_get('max_execution_time');
+        ini_set('display_errors', 'On');
+        $t1=time();
+
         $filmrecherche = $_POST['Film'];
 
         $object = json_decode(file_get_contents("http://www.omdbapi.com/?s=$filmrecherche"));
@@ -10,20 +14,25 @@
         foreach($object->Search as $Film){
 
                 echo '<p>', $Film->Title, '</p></br>';
-		$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
+                $details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
                 $posterURL = $details->Poster;
 
                 if ($posterURL != "N/A"){
 
-                        //$posterFile = basename($posterURL);
-                        //file_put_contents(tempPoster/$posterFile, fopen($posterURL, 'r'));
-                        echo '<img src="', $posterURL, '"></br>';
+                        $posterFile = basename($posterURL);
+                        file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
+                        echo '<img src="', "tempPoster/$posterFile", '"></br>';
 
                 }
 
 
 
         }
+
+        $t2=time();
+
+        $t_lapsed=$t2-$t1;
+        echo "Temps d'execution = $t_lapsed";
 
 ?>
 
