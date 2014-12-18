cf9c69d41cb79de7fb36b9aad4133d309efd104b
Paul TREHIOU <paul.trehiou@utbm.fr>
2014-12-18 14:15:25 +0100

    Corrections pour l'ajout d'un film

--- www/enregistrerfilm.php
+++ www/enregistrerfilm.php
@@ -1,24 +1,32 @@
 <?php
 
-	$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$_GET['detailsfilm']&plot=full&r=json"));
+        ini_set('display_errors', 'On');
 
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e)
-	{
-		die('Erreur : ' . $e->getMessage());
-	}
+        echo "ça marche !";
 
-	$req = $bdd->prepare('INSERT INTO FILM VALUES(NULL, :titre, :synopsis, :sortie, :affiche, NULL, :age)');
-	$req->execute(array(
-		'titre' => $details->Title,
-		'synopsis' => $details->Plot,
-		'sortie' => date(Y-m-d,strtotime($details->Released)),
-		'affiche' => tempPoster/basename($details->Poster),
-		'age' => $details->Rated
-	));
+
+        $details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$_GET['detailsfilm']&plot=full&r=json"));
+
+        $sortie = date(Y-m-d,strtotime($details->Released));
+        $poster = tempPoster/basename($details->Poster);
+
+        try
+        {
+                $bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+
+        $req = $bdd->prepare('INSERT INTO FILM VALUES(NULL, :titre, :synopsis, :sortie, :affiche, NULL, :age)');
+        $req->execute(array(
+                'titre' => $details->Title,
+                'synopsis' => $details->Plot,
+                'sortie' => $sortie,
+                'affiche' => $poster,
+                'age' => $details->Rated
+        ));
 
 
 
