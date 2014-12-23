e2f89a350197ee5d2756981845a28179dce6eaf6
Paul Trehiou <paul.trehiou@utbm.fr>
2014-12-23 15:09:02 +0100

    Changement de méthode pour l'association entre acteurs et film

--- www/enregistrerfilm.php
+++ www/enregistrerfilm.php
@@ -61,23 +61,15 @@
 					'prenom' => $curacteur[0]
 				));
 
-				$artid = $bdd->lastInsertId('ARTISTE');
 
-				if(!isset($artid))
-				{
-
-					$reponse = $bdd->query('SELECT ART_ID FROM `ARTISTE` WHERE ART_NOM = $curacteur[1] AND ART_PRENOM = $curacteur[0]');
-					$artid = $reponse->fetch();
-
-				}
+				$reponse = $bdd->query('SELECT ART_ID FROM `ARTISTE` WHERE ART_NOM = $curacteur[1] AND ART_PRENOM = $curacteur[0]');
+				$artid = $reponse->fetch();
 
 				$req = $bdd->prepare('INSERT INTO AVOIR_JOUE_DANS VALUES(:idfilm, :idart)');
 				$req->execute(array(
 					'idfilm' => $filmid,
 					'idart' => $artid
 				));
-
-				unset($artid);
 
 			}
 
@@ -88,15 +80,8 @@
 				'prenom' => $directeur[0]
 			));
 
-			$artid = $bdd->lastInsertId('ARTISTE');
-
-			if(!isset($artid))
-			{
-
-				$reponse = $bdd->query('SELECT ART_ID FROM `ARTISTE` WHERE ART_NOM = $directeur[1] AND ART_PRENOM = $directeur[0]');
-				$artid = $reponse->fetch();
-
-			}
+			$reponse = $bdd->query('SELECT ART_ID FROM `ARTISTE` WHERE ART_NOM = $directeur[1] AND ART_PRENOM = $directeur[0]');
+			$artid = $reponse->fetch();
 
 			$req = $bdd->prepare('INSERT INTO REALISER VALUES(:idart, :idfilm)');
 			$req->execute(array(
