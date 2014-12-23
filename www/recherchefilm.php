e5cc38b748254efa0a8cfc2692b54beef6debb8e
Paul Trehiou <paul.trehiou@utbm.fr>
2014-12-23 12:00:30 +0100

    Désactivation de l'affichage de variables de debug

--- www/recherchefilm.php
+++ www/recherchefilm.php
@@ -3,36 +3,35 @@
 
 <?php
 
-        echo ini_get('max_execution_time');
-        ini_set('display_errors', 'On');
-        $t1=time();
+	ini_set('display_errors', 'On');
+	$t1=time();
 
-        $filmrecherche = urlencode($_POST['Film']);
+	$filmrecherche = urlencode($_POST['Film']);
 
-        $object = json_decode(file_get_contents("http://www.omdbapi.com/?s=$filmrecherche"));
+	$object = json_decode(file_get_contents("http://www.omdbapi.com/?s=$filmrecherche"));
 
-        foreach($object->Search as $Film){
+	foreach($object->Search as $Film){
 
 		$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
-                echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
-                $posterURL = $details->Poster;
+		echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
+		$posterURL = $details->Poster;
 
-                if ($posterURL != "N/A"){
+		if ($posterURL != "N/A"){
 
-                        $posterFile = basename($posterURL);
-                        if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
-                        echo '<img src="', "tempPoster/$posterFile", '"></br>';
+			$posterFile = basename($posterURL);
+			if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
+			echo '<img src="', "tempPoster/$posterFile", '"></br>';
 
-                }
+		}
 
 
 
-        }
+	}
 
-        $t2=time();
+	$t2=time();
 
-        $t_lapsed=$t2-$t1;
-        echo "Temps d'execution = $t_lapsed";
+	$t_lapsed=$t2-$t1;
+	echo "Temps d'execution = $t_lapsed";
 
 ?>
 
