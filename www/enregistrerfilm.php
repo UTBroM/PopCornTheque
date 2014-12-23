7b1d553252925852e185d4bdfb1fa88c724dcc60
Paul Trehiou <paul.trehiou@utbm.fr>
2014-12-23 11:44:37 +0100

    L'enregistrement d'un film à partir de son id fonctionne

--- www/enregistrerfilm.php
+++ www/enregistrerfilm.php
@@ -1,34 +1,36 @@
 <?php
 
-        ini_set('display_errors', 'On');
+	ini_set('display_errors', 'On');
 
-        echo "ça marche !";
+	echo "ça marche !";
 
+	$idfilm = $_GET['detailsfilm'];
 
-        $details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$_GET['detailsfilm']&plot=full&r=json"));
+	$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$idfilm&plot=full&r=json"));
+	$imagename = basename($details->Poster);
+	$sortie = date("Y-m-d",strtotime($details->Released));
+	$poster = "tempPoster/$imagename";
 
-        $sortie = date("Y-m-d",strtotime($details->Released));
-        $poster = tempPoster/basename($details->Poster);
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
 
-        try
-        {
-                $bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-        }
-        catch (Exception $e)
-        {
-                die('Erreur : ' . $e->getMessage());
-        }
-
-        $req = $bdd->prepare('INSERT INTO FILM VALUES(NULL, :titre, :synopsis, :sortie, :affiche, NULL, :age)');
-        $req->execute(array(
-                'titre' => $details->Title,
-                'synopsis' => $details->Plot,
-                'sortie' => $sortie,
-                'affiche' => $poster,
-                'age' => $details->Rated
-        ));
+	$req = $bdd->prepare('INSERT INTO FILM VALUES(NULL, :titre, :synopsis, :sortie, :affiche, NULL, :age)');
+	$req->execute(array(
+		'titre' => $details->Title,
+		'synopsis' => $details->Plot,
+		'sortie' => $sortie,
+		'affiche' => $poster,
+		'age' => $details->Rated
+	));
 
 
 
 ?>
+
 
