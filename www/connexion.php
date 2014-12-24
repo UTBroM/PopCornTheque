fc9f7b92956329986edc1b1139638710f9aeb29a
Paul Trehiou <paul.trehiou@utbm.fr>
2014-12-24 10:28:52 +0100

    Premier test de connexion d'un utilisateur

---
+++ www/connexion.php
@@ -0,0 +1,35 @@
+<?php
+
+	ini_set('display_errors', 'On');
+
+	$user = $_POST['user'];
+	$password = sha1($_POST['password']);
+
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
+
+	$req = $bdd->prepare('SELECT UTI_MOT_DE_PASSE FROM UTILISATEURS WHERE UTI_ID = :nom');
+	$req->execute(array(
+		'nom' => $user
+	));
+
+	$realpassword = $donnees = $req->fetch()[0];
+
+	if ($realpassword = $password){
+
+		echo "Bon mot de passe";
+
+	}
+	else{
+
+		echo "Mauvais mot de passe";
+
+	}
+
+?>
