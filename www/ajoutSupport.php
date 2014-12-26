2cb0ea9cf89a228f651b2c83e16752857a450aaa
Victor SENE <victor.sene@utbm.fr>
2014-12-26 20:46:28 +0100

    Modification ajoutSupport.php

--- www/ajoutSupport.php
+++ www/ajoutSupport.php
@@ -1,7 +1,11 @@
 <?php
 	
 	ini_set('display_errors', 'On');
-	
+
+	$support_id = $_POST['support_id'];
+	$utilisateur_name = $_POST['utilisateur_name'];
+	$support_nom = $_POST['support_nom'];
+	$support_nbr = $_POST['support_nbr'];
 
 	try{
 		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
@@ -12,8 +16,10 @@
 
 	$req = $bdd->prepare('INSERT INTO SUPPORT VALUES(NULL, :support_id, :utilisateur_name, :support_nom, :support_nbr)');
 	$req->execute(array(
-		'support_nom' => ,
-
-		));
+		'support_id' => $support_id,
+		'utilisateur_name' => $utilisateur_name,
+		'support_nom' => $support_nom,
+		'support_nbr' => $support_nbr,
+	));
 
 ?>
