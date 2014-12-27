94c6b2ee5d24610ae9defc19858923a69b32ed12
Victor SENE <victor.sene@utbm.fr>
2014-12-28 00:00:25 +0100

    Modification de emprunt.php

--- www/emprunt.php
+++ www/emprunt.php
@@ -3,8 +3,10 @@
 	ini_set('display_errors', 'On');
 
 	$emprunter_id = $_POST['emprunter_id'];
+	$current_id_user = ;
 	$emprunt_date = $_POST['emprunt_date'];
 	$retour_emprunt_date = $_POST['retour_emprunt_date'];
+	$emprunt_duree = $_POST['emprunt_duree'];
 
 
 	try{
@@ -14,10 +16,5 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('UPDATE SUPPORT SET UTI_ID_EMPRUNT VALUES(:emprunter_id, :emprunt_date, :retour_emprunt_date)');
-	$req->execute(array(
-		'emprunter_id' => $emprunter_id,
-		'emprunt_date' => $emprunt_date,
-		'retour_emprunt_date' => $retour_emprunt_date,
-	));
+	$req = $bdd->exec('UPDATE EMPRUNT SET EMPR_ID = NULL, UTI_ID = $current_id_user , SUP_ID = $current_id_support, EMPR_DATE = date(Y-M-D H:M:S), EMPR_RETOUR_THEORIQUE = $retour_emprunt_date, EMPR_RETOUR_REEL = ($retour_emprunt_date - date(Y-M-D H:M:S), EMPR_DUREE = $emprunt_duree, EMPR_RENDU = "non"');
 ?>
