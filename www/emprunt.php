38979bac9f4e38c3c11ca14d3085594d7c612700
Victor SENE <victor.sene@utbm.fr>
2014-12-28 00:17:57 +0100

    modification emprunt.php
    ça devrait mieux coller maintenant. Merci de tester sur votre base

--- www/emprunt.php
+++ www/emprunt.php
@@ -2,11 +2,15 @@
 	
 	ini_set('display_errors', 'On');
 
-	$emprunter_id = $_POST['emprunter_id'];
-	$current_id_user = ;
+	$current_id_user = $_POST['current_id_user'];
 	$emprunt_date = $_POST['emprunt_date'];
+	$current_id_support = $_POST['current_id_support'];
+	$currt_date = date(Y-M-D H:M:S);
+	
 	$retour_emprunt_date = $_POST['retour_emprunt_date'];
-	$emprunt_duree = $_POST['emprunt_duree'];
+	$retour_emprunt_date_reel = NULL;
+	$emprunt_duree = $retour_emprunt_date - $date;
+	$rendu = 'non';
 
 
 	try{
@@ -16,5 +20,21 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->exec('UPDATE EMPRUNT SET EMPR_ID = NULL, UTI_ID = $current_id_user , SUP_ID = $current_id_support, EMPR_DATE = date(Y-M-D H:M:S), EMPR_RETOUR_THEORIQUE = $retour_emprunt_date, EMPR_RETOUR_REEL = ($retour_emprunt_date - date(Y-M-D H:M:S), EMPR_DUREE = $emprunt_duree, EMPR_RENDU = "non"');
+	$req = $bdd->prepare('UPDATE EMPRUNT SET EMPR_ID = NULL, UTI_ID = :current_id_user , SUP_ID = :current_id_support, EMPR_DATE = :currt_date, EMPR_RETOUR_THEORIQUE = :retour_emprunt_date, EMPR_RETOUR_REEL = :retour_emprunt_date_reel, EMPR_DUREE = :emprunt_duree, EMPR_RENDU = :rendu');
+	$req = execute(array(
+		'current_id_user' => $current_id_user,
+		'emprunt_date' => $emprunt_date,
+		'current_id_support' => $current_id_support,
+		'currt_date' => $currt_date,
+		'retour_emprunt_date' => $retour_emprunt_date,
+		'retour_emprunt_date_reel' => $retour_emprunt_date_reel,
+		'emprunt_duree' => $emprunt_duree,
+		'rendu' => $rendu,
+	));
+
+	$req2 = $bdd->prepare('DELETE FROM DEMANDE_EMPRUNT WHERE SUP_ID = :current_id_support AND UTI_ID = :current_id_user');
+	$req2 = execute(array(
+		'current_id_support' => $current_id_support,
+		'current_id_user' => $current_id_user,
+	));
 ?>
