4a15c103fc0de81f8b86666994650ed723246f19
Victor SENE <victor.sene@utbm.fr>
2014-12-28 16:17:58 +0100

    modification ajoutAmi.php

--- www/ajoutAmi.php
+++ www/ajoutAmi.php
@@ -1,7 +1,8 @@
 <?php
 	
 	ini_set('display_errors', 'On');
-
+	$target_user_id = $_POST['target_user_id'];
+	$current_user_id = $_POST['current_user_id'];
 
 	try{
 		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
@@ -10,9 +11,10 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('INSERT INTO ETRE_AMI VALUES(NULL, )');
+	$req = $bdd->prepare('INSERT INTO ETRE_AMI VALUES(:target_user_id, :current_user_id)');
 	$req = execute(array(
-
+		'target_user_id' => $target_user_id,
+		'current_user_id' => $current_user_id,
 	));
 
 ?>
