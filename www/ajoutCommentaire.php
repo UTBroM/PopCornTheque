e0728c9398395c4edfb7a3996964425b28d95e97
Victor SENE <victor.sene@utbm.fr>
2014-12-28 14:52:40 +0100

    création ajoutCommentaire.php

---
+++ www/ajoutCommentaire.php
@@ -0,0 +1,26 @@
+<?php
+	
+	ini_set('display_errors', 'On');
+	$film_id = $_POST['film_id'];
+	$user_id = $_POST['user_id'];
+	$commentaire = $_POST['commentaire'];
+	$note = $_POST['note'];
+	$date_actuelle = $_POST['date_actuelle'];
+
+	try{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e){
+		die('Erreur : ' . $e->getMessage());
+	}
+
+	$req = $bdd->prepare('INSERT INTO COMMENTAIRE_FILM VALUES(NULL, :film_id, :user_id, :commentaire, :note, :date_actuelle)');
+	$req = execute(array(
+		'film_id' => $film_id,
+		'user_id' => $user_id,
+		'commentaire' => $commentaire,
+		'note' => $note,
+		'date_actuelle' => $date_actuelle,
+	));
+
+?>
