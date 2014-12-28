be8125d53a683f0c69c82026bb5a33809f91f4c3
Victor SENE <victor.sene@utbm.fr>
2014-12-28 14:56:31 +0100

    creation .php ajout commentaire sur un utilisateur

---
+++ www/ajoutCommentaireUser.php
@@ -0,0 +1,19 @@
+<?php
+	
+	ini_set('display_errors', 'On');
+
+
+	try{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e){
+		die('Erreur : ' . $e->getMessage());
+	}
+
+	$req = $bdd->prepare('');
+	$req = execute(array(
+
+
+	));
+
+?>
