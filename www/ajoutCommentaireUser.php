7641145fe49ee4572ebd311db2a0490dd6d1b6e1
Victor SENE <victor.sene@utbm.fr>
2014-12-28 15:28:16 +0100

    modification ajoutCommentaireUser.php

--- www/ajoutCommentaireUser.php
+++ www/ajoutCommentaireUser.php
@@ -1,7 +1,12 @@
 <?php
 	
 	ini_set('display_errors', 'On');
-
+	$film_id = $_POST['film_id'];
+	$target_user_id = $_POST['target_user_id'];
+	$current_user_id = $_POST['current_user_id'];
+	$commentaire = $_POST['commentaire'];
+	$note = $_POST['note'];
+	$date_actuelle = $_POST['date_actuelle'];
 
 	try{
 		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
@@ -10,10 +15,14 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('');
+	$req = $bdd->prepare('INSERT INTO COMMENTAIRES_UT VALUES(NULL, :film_id, :target_user_id, :current_user_id, :commentaire, :note, :date_actuelle)');
 	$req = execute(array(
-
-
+		'film_id' => $film_id,
+		'target_user_id' => $target_user_id,
+		'current_user_id' => $current_user_id,
+		'commentaire' => $commentaire,
+		'note' => $note,
+		'date_actuelle' => $date_actuelle,
 	));
 
 ?>
