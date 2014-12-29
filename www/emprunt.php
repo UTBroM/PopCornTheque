702c3441feb75d590e9a5d1a885db5b9dcaa80e2
Victor SENE <victor.sene@utbm.fr>
2014-12-29 14:17:43 +0100

    variable inutile

--- www/emprunt.php
+++ www/emprunt.php
@@ -3,7 +3,6 @@
 	ini_set('display_errors', 'On');
 
 	$current_id_user = $_POST['current_id_user'];
-	$emprunt_date = $_POST['emprunt_date'];
 	$current_id_support = $_POST['current_id_support'];
 	$currt_date = date(Y-M-D H:M:S);
 	
@@ -23,7 +22,6 @@
 	$req = $bdd->prepare('UPDATE EMPRUNT SET EMPR_ID = NULL, UTI_ID = :current_id_user , SUP_ID = :current_id_support, EMPR_DATE = :currt_date, EMPR_RETOUR_THEORIQUE = :retour_emprunt_date, EMPR_RETOUR_REEL = :retour_emprunt_date_reel, EMPR_DUREE = :emprunt_duree, EMPR_RENDU = :rendu');
 	$req = execute(array(
 		'current_id_user' => $current_id_user,
-		'emprunt_date' => $emprunt_date,
 		'current_id_support' => $current_id_support,
 		'currt_date' => $currt_date,
 		'retour_emprunt_date' => $retour_emprunt_date,
