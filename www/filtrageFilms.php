84f0c5263e6ee3e45e4808aa9a8e89188bfb3e8e
Paul Trehiou <paul.trehiou@utbm.fr>
2014-12-31 19:09:18 +0100

    Correction filtrage des films

--- www/filtrageFilms.php
+++ www/filtrageFilms.php
@@ -4,23 +4,23 @@
 
 	$film = $_POST['film'];
 
+	echo "Recherche du film ", htmlspecialchars($film);
+
 	try
 	{
-	$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
 	}
 	catch (Exception $e)
 	{
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('SELECT *  FROM `FILM` WHERE `FILM_TITRE` LIKE '%:film%'');
-	$req->execute(array(
-		'film' => $film
-	));
+	$req = $bdd->prepare("SELECT *  FROM FILM WHERE FILM_TITRE LIKE CONCAT('%', ?, '%')");
+	$req->execute(array($film));
 
-	while($donnees = $reponse->fetch()){
+	while($donnees = $req->fetch()){
 
-		echo htmlspecialchars($donnees['FILM_TITRE']), '<br />';
+		echo '<br />', htmlspecialchars($donnees['FILM_TITRE']), '<br />';
 
 	}
 
