50eee52b8cfe281d4591da7090ea29ddd6e5bf1b
Victor SENE <victor.sene@utbm.fr>
2015-01-01 20:07:39 +0100

    adaptation requête sql pour affichage affiche film

--- www/listeFilmsUser.php
+++ www/listeFilmsUser.php
@@ -13,11 +13,14 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('SELECT * FROM SUPPORT WHERE UTI_ID = ?');
+	$req = $bdd->prepare('SELECT FILM_ID, UTI_ID, SUP_NOM  FROM SUPPORT =S WHERE UTI_ID = ? IN (SELECT FILM_ID FILM_AFFICHE FROM FILM = F WHERE S.FILM_ID = F.FILM_ID)');
 	$req->execute(array($utilisateur_id));
 
+
+	echo " Nom d'utilisateur 	Id du film 		Nom du support"
 	while($donnees = $req->fetch()){
-		echo '</br>', $donnees['UTI_ID'], '</br>', $donnees['FILM_ID'], '</br>', $donnees['SUP_NOM'], '</br>';
+		echo '</br>', htmlspecialchars($donnees['UTI_ID']),' ' ,htmlspecialchars($donnees['FILM_ID']),' ',htmlspecialchars($donnees['SUP_NOM']), '</br>';
+		echo '<img src="', $donnees['FILM_AFFICHE'], '</br>';
 	}
 
 ?>
