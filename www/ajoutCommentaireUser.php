f2d862b7a43cc9f309c8a96dcdd3edd96edeecbb
Paul Trehiou <paul.trehiou@utbm.fr>
2015-01-01 17:54:00 +0100

    Suppression du debug

--- www/ajoutCommentaireUser.php
+++ www/ajoutCommentaireUser.php
@@ -7,7 +7,7 @@
 	$note = $_POST['note'];
 	$date_actuelle = date("Y-m-d H:i:s");
 
-	echo $target_user_id, $current_user_id, $commentaire, $note, $date_actuelle;
+	echo "Sauvegarde du commentaire terminé";
 
 	try{
 		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
