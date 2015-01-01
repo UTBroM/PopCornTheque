bc816954ceea7996cee92ded5a2a778dda468a24
Paul Trehiou <paul.trehiou@utbm.fr>
2015-01-01 17:36:02 +0100

    Correction erreur syntaxe

--- www/ajoutCommentaireUser.php
+++ www/ajoutCommentaireUser.php
@@ -5,7 +5,7 @@
 	$current_user_id = $_POST['current_user_id'];
 	$commentaire = $_POST['commentaire'];
 	$note = $_POST['note'];
-	$date_actuelle = date(Y-m-d H:i:s);
+	$date_actuelle = date("Y-m-d H:i:s");
 
 	echo $target_user_id, $current_user_id, $commentaire, $note, $date_actuelle;
 
