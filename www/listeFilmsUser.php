b683ce3ca75d29ac64c8fb0a6dbf5be2b84f8e1e
Victor SENE <victor.sene@utbm.fr>
2015-01-01 20:11:52 +0100

    ambiguïté dans tableau résultat

--- www/listeFilmsUser.php
+++ www/listeFilmsUser.php
@@ -13,13 +13,13 @@
 		die('Erreur : ' . $e->getMessage());
 	}
 
-	$req = $bdd->prepare('SELECT FILM_ID, UTI_ID, SUP_NOM  FROM SUPPORT =S WHERE UTI_ID = ? IN (SELECT FILM_ID FILM_AFFICHE FROM FILM = F WHERE S.FILM_ID = F.FILM_ID)');
+	$req = $bdd->prepare('SELECT FILM_ID, UTI_ID, SUP_NOM  FROM SUPPORT = S WHERE UTI_ID = ? IN (SELECT FILM_ID, FILM_AFFICHE FROM FILM = F WHERE S.FILM_ID = F.FILM_ID)');
 	$req->execute(array($utilisateur_id));
 
 
 	echo " Nom d'utilisateur 	Id du film 		Nom du support"
 	while($donnees = $req->fetch()){
-		echo '</br>', htmlspecialchars($donnees['UTI_ID']),' ' ,htmlspecialchars($donnees['FILM_ID']),' ',htmlspecialchars($donnees['SUP_NOM']), '</br>';
+		echo '</br>', htmlspecialchars($donnees['UTI_ID']),' ' ,htmlspecialchars($donnees['S.FILM_ID']),' ',htmlspecialchars($donnees['SUP_NOM']), '</br>';
 		echo '<img src="', $donnees['FILM_AFFICHE'], '</br>';
 	}
 
