d0892d0ec28c369783e0a3e90664492eb97ddeb9
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 09:58:21 +0100

    ajoutAmi.php connexion obligatoire

--- www/ajoutAmi.php
+++ www/ajoutAmi.php
@@ -1,7 +1,5 @@
 <?php
 
-	ini_set('display_errors', 'On');
-
 	session_start();
 	if ((!isset($_SESSION['login'])) || (empty($_SESSION['login'])))
 	{
@@ -10,22 +8,49 @@
 		exit();
 	}
 
-	$target_user_id = $_POST['target_user_id'];
-	$current_user_id = $_POST['current_user_id'];
-
-	echo "Demande d'ami(e) effectuée";
-
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
-
-	$req = $bdd->prepare('INSERT INTO ETRE_AMI VALUES(:target_user_id, :current_user_id)');
-	$req->execute(array(
-		'target_user_id' => $target_user_id,
-		'current_user_id' => $current_user_id,
-	));
-
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	<link rel="stylesheet" href="style.css" />
+	<title>PopcornTeque</title>
+	</head>
+
+	<body>
+
+	<?php
+
+		ini_set('display_errors', 'On');
+
+		session_start();
+		if ((!isset($_SESSION['login'])) || (empty($_SESSION['login'])))
+		{
+			// la variable 'login' de session est non déclaré ou vide
+			header('Location: index.php'); 
+			exit();
+		}
+
+		$target_user_id = $_POST['target_user_id'];
+		$current_user_id = $_POST['current_user_id'];
+
+		echo "Demande d'ami(e) effectuée";
+
+		try{
+			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		}
+		catch (Exception $e){
+			die('Erreur : ' . $e->getMessage());
+		}
+
+		$req = $bdd->prepare('INSERT INTO ETRE_AMI VALUES(:target_user_id, :current_user_id)');
+		$req->execute(array(
+			'target_user_id' => $target_user_id,
+			'current_user_id' => $current_user_id,
+		));
+
+	?>
+
+	</body>
+</html>
