291fccb3ff3688631ae70bd38628800bd4da9fc4
Victor SENE <victor@sene.ovh>
2015-01-06 14:00:53 +0100

    correction erreur chargement css

--- www/demandeEmprunt.php
+++ www/demandeEmprunt.php
@@ -1,24 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
 	
-	ini_set('display_errors', 'On');
+	<body>
+	<?php
+		
+		ini_set('display_errors', 'On');
 
-	$utilisateur_id = $_POST['utilisateur_id'];
-	$support_id = $_POST['support_id'];
+		$utilisateur_id = $_POST['utilisateur_id'];
+		$support_id = $_POST['support_id'];
 
-	echo "Demande d'emprunt effectuée";
+		echo "Demande d'emprunt effectuée";
 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
+		try{
+			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		}
+		catch (Exception $e){
+			die('Erreur : ' . $e->getMessage());
+		}
 
-	$req = $bdd->prepare('INSERT INTO DEMANDE_EMPRUNT VALUES(:utilisateur_id, :support_id)');
-	$req->execute(array(
-		'utilisateur_id' => $utilisateur_id,
-		'support_id' => $support_id,
-	));
+		$req = $bdd->prepare('INSERT INTO DEMANDE_EMPRUNT VALUES(:utilisateur_id, :support_id)');
+		$req->execute(array(
+			'utilisateur_id' => $utilisateur_id,
+			'support_id' => $support_id,
+		));
 
 
-?>
+	?>
+
+	</body>
+</html>
