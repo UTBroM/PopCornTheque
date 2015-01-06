95497b60cfada57bd8c45857a0a5a74df4428e4b
Victor SENE <victor@sene.ovh>
2015-01-06 20:12:17 +0100

    userSpace in progress ...

--- www/userSpace.php
+++ www/userSpace.php
@@ -44,17 +44,77 @@
 			$datenaissance = date("d/m/Y", strtotime($dateMySQL));
 
 		?>
-		<p>
-			<h2>Mes infos<br/></h2>
-			Nom d'utilisateur : <?php echo htmlspecialchars($donnees2['UTI_ID']); ?><br />
-			Nom : <?php echo htmlspecialchars($donnees2['UTI_NOM']); ?><br />
-			Prénom <?php echo htmlspecialchars($donnees2['UTI_PRENOM']); ?><br />
-			Date de naissance : <?php echo $datenaissance; ?><br />
-			Rue : <?php echo htmlspecialchars($donnees2['UTI_RUE']); ?><br />
-			Code postal : <?php echo $donnees2['UTI_CODE_POSTAL']; ?><br />
-			Ville : <?php echo htmlspecialchars($donnees2['UTI_VILLE']); ?><br />
-			eMail : <?php echo htmlspecialchars($donnees2['UTI_MAIL']); ?>
-		</p>
+		<section>
+			<p>
+				<h2>Mes infos<br/></h2>
+				Nom d'utilisateur : <?php echo htmlspecialchars($donnees2['UTI_ID']); ?><br />
+				Nom : <?php echo htmlspecialchars($donnees2['UTI_NOM']); ?><br />
+				Prénom <?php echo htmlspecialchars($donnees2['UTI_PRENOM']); ?><br />
+				Date de naissance : <?php echo $datenaissance; ?><br />
+				Rue : <?php echo htmlspecialchars($donnees2['UTI_RUE']); ?><br />
+				Code postal : <?php echo $donnees2['UTI_CODE_POSTAL']; ?><br />
+				Ville : <?php echo htmlspecialchars($donnees2['UTI_VILLE']); ?><br />
+				eMail : <?php echo htmlspecialchars($donnees2['UTI_MAIL']); ?>
+			</p>
+		</section>
+		
+		<section>
+			<p>
+				<h2>Mes demande d'amis<br/></h2>
+				<?php 
+					$req3 = $bdd->prepare("SELECT * FROM ETRE_AMI WHERE UTI_ID_SOURCE = ? ");
+					$req3->execute(array($user));
+
+					while($donnees3 = $req3->fetch()){
+						echo htmlspecialchars($donnees3['UTI_ID_CIBLE']), "<br/>";
+					}
+					
+				?>
+				<h2>Mes invitations</h2>
+				<?php 
+					$req4 = $bdd->prepare("SELECT * FROM ETRE_AMI WHERE UTI_ID_CIBLE = ? ");
+					$req4->execute(array($user));
+
+					while($donnees4 = $req4->fetch()){
+						echo htmlspecialchars($donnees4['UTI_ID_SOURCE']), "<br/>";
+					}
+				?>
+			</p>
+		</section>
+
+		<section>
+			<p>
+				<h2>Mes emprunts<br/></h2>
+				<?php 
+					$req5 = $bdd->prepare("SELECT * FROM EMPRUNT WHERE UTI_ID = ? ");
+					$req5->execute(array($user));
+
+					while($donnees5 = $req5->fetch()){
+						echo htmlspecialchars($donnees5['SUP_ID']), "à rendre le ", htmlspecialchars($donnees5['EMPR_RETOUR_THEORIQUE']), "<br/>";
+					}
+				?>
+			</p>
+		</section>
+
+		<section>
+			<p>
+				<h2>Demande d'empreint<br/></h2>
+				<?php 
+					$req5 = $bdd->prepare("SELECT S.SUP_ID, F.FILM_ID, F.FILM_TITRE FROM DEMANDE_EMPRUNT AS DE 
+													INNER JOIN SUPPORT AS S 
+														ON DE.SUP_ID = S.SUP_ID 
+													INNER JOIN FILM AS F 
+														ON S.FILM_ID = F.FILM_ID  
+													WHERE DE.UTI_ID = ? ");
+					$req5->execute(array($user));
+					echo "Vous avez demandé :<br/>";
+					while($donnees5 = $req5->fetch()){
+						echo htmlspecialchars($donnees5['SUP_ID']), " : ", htmlspecialchars($donnees5['F.FILM_TITRE']), "<br/>";
+					}
+				?>
+
+			</p>
+		</section>
 
 	</body>
 </html>
