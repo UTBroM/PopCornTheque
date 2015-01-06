3a438fe84eb6b6948cae42eed263bcbcbe2f077a
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 19:48:13 +0100

    N'afficher que les film

--- www/recherchefilm.php
+++ www/recherchefilm.php
@@ -21,19 +21,22 @@
 
 			foreach($object->Search as $Film){
 
-				$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
-				echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
-				$posterURL = $details->Poster;
+				if($Film->Type == "movie"){
 
-				if ($posterURL != "N/A"){
+					$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
 
-					$posterFile = basename($posterURL);
-					if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
-					echo '<img src="', "tempPoster/$posterFile", '"></br>';
+					echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
+					$posterURL = $details->Poster;
+
+					if ($posterURL != "N/A"){
+
+						$posterFile = basename($posterURL);
+						if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
+						echo '<img src="', "tempPoster/$posterFile", '"></br>';
+
+					}
 
 				}
-
-
 
 			}
 
