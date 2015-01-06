51992bc2d23442f847f638d553c5f0f6355647e8
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 16:33:07 +0100

    Ne sert plus à rien

--- www/header.php
+++ www/header.php
@@ -17,7 +17,6 @@
 	?>
 		<li><a href="ajoutAmi.html">Ajouter un(e) ami(e)</a></li>
 		<li><a href="ajoutCommentaireUser.html">Ajouter un commentaire sur un user</a></li>
-		<li><a href="demandeEmprunt.html">Faire une demande d'emprunt</a></li>
 		<li><a href="emprunt.html">Emprunter</a></li>
 		<li><a href="listeFilmsUser.html">Liste des Films par user</a></li>
 		<li><a href="filtrageFilms.html">Filtrage</a></li>
