2d45f47b975476803b4e954d84b398eb5c79fa89
Victor SENE <victor@sene.ovh>
2015-01-06 13:57:09 +0100

    ajout des balises html

--- www/listeFilmsUser.php
+++ www/listeFilmsUser.php
@@ -1,28 +1,39 @@
-<?php
-	
-	ini_set('dispay_errors', 'On');
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
 
-	$utilisateur_id = $_POST['utilisateur_id'];
+	<body>
+	<?php
+		
+		ini_set('dispay_errors', 'On');
 
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e)
-	{
-		die('Erreur : ' . $e->getMessage());
-	}
+		$utilisateur_id = $_POST['utilisateur_id'];
 
-	$req = $bdd->prepare('SELECT F.FILM_ID, SUP_NOM, FILM_AFFICHE, FILM_TITRE FROM SUPPORT AS S INNER JOIN FILM AS F ON F.FILM_ID = S.FILM_ID WHERE UTI_ID = ?');
-	$req->execute(array($utilisateur_id));
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		}
+		catch (Exception $e)
+		{
+			die('Erreur : ' . $e->getMessage());
+		}
 
-	echo "<table>\n<caption>Liste des support de ", htmlspecialchars($utilisateur_id),"</caption>\n";
-	echo "<thead>\n<tr>\n<th>Id du film\n<th>Titre\n<th>Nom du support\n<th>\n";
-	echo "<tbody>\n";
-	while($donnees = $req->fetch()){
-		echo "<tr>\n<td>",$donnees['FILM_ID'],"\n<td>",$donnees['FILM_TITRE'],"\n<td>",htmlspecialchars($donnees['SUP_NOM']),"\n";
-		echo '<td><img src="', $donnees['FILM_AFFICHE'], '" width=40% height=40%>';
-	}
-	echo"</table>";
+		$req = $bdd->prepare('SELECT F.FILM_ID, SUP_NOM, FILM_AFFICHE, FILM_TITRE FROM SUPPORT AS S INNER JOIN FILM AS F ON F.FILM_ID = S.FILM_ID WHERE UTI_ID = ?');
+		$req->execute(array($utilisateur_id));
 
-?>
+		echo "<table>\n<caption>Liste des support de ", htmlspecialchars($utilisateur_id),"</caption>\n";
+		echo "<thead>\n<tr>\n<th>Id du film\n<th>Titre\n<th>Nom du support\n<th>\n";
+		echo "<tbody>\n";
+		while($donnees = $req->fetch()){
+			echo "<tr>\n<td>",$donnees['FILM_ID'],"\n<td>",$donnees['FILM_TITRE'],"\n<td>",htmlspecialchars($donnees['SUP_NOM']),"\n";
+			echo '<td><img src="', $donnees['FILM_AFFICHE'], '" width=40% height=40%>';
+		}
+		echo"</table>";
+
+	?>
+	</body>
+</html>
