fcb84a35bc3ec41cc9b87d351591265c2c79b976
Victor SENE <victor@sene.ovh>
2015-01-06 14:14:17 +0100

    ajout head et css

--- www/ajoutCommentaireFilm.php
+++ www/ajoutCommentaireFilm.php
@@ -1,36 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>PopcornTeque</title>
+    </head>
 
-	session_start();
-	if ((!isset($_SESSION['login'])) || (empty($_SESSION['login'])))
-	{
-		// la variable 'login' de session est non déclaré ou vide
-		header('Location: index.php'); 
-		exit();
-	}
+    <body>
+    	<header>
+            <a href="index.php">
+            	<img src="images/logo.png" height="120px">
+            </a>
+    	</header>
 
-	ini_set('display_errors', 'On');
-	$film_id = $_POST['film_id'];
-	$user_id = $_SESSION['login'];
-	$commentaire = $_POST['commentaire'];
-	$note = $_POST['note'];
-	$date_actuelle = date("Y-m-d H:i:s");
+		<?php
 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
+			session_start();
+			if ((!isset($_SESSION['login'])) || (empty($_SESSION['login'])))
+			{
+				// la variable 'login' de session est non déclaré ou vide
+				header('Location: index.php'); 
+				exit();
+			}
 
-	$req = $bdd->prepare('INSERT INTO COMMENTAIRES_FILM VALUES(NULL, :film_id, :user_id, :commentaire, :note, :date_actuelle)');
-	$req->execute(array(
-		'film_id' => $film_id,
-		'user_id' => $user_id,
-		'commentaire' => $commentaire,
-		'note' => $note,
-		'date_actuelle' => $date_actuelle,
-	));
+			ini_set('display_errors', 'On');
+			$film_id = $_POST['film_id'];
+			$user_id = $_SESSION['login'];
+			$commentaire = $_POST['commentaire'];
+			$note = $_POST['note'];
+			$date_actuelle = date("Y-m-d H:i:s");
 
-	header("Location: ".$_SERVER['HTTP_REFERER']."");
+			try{
+				$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+			}
+			catch (Exception $e){
+				die('Erreur : ' . $e->getMessage());
+			}
 
-?>
+			$req = $bdd->prepare('INSERT INTO COMMENTAIRES_FILM VALUES(NULL, :film_id, :user_id, :commentaire, :note, :date_actuelle)');
+			$req->execute(array(
+				'film_id' => $film_id,
+				'user_id' => $user_id,
+				'commentaire' => $commentaire,
+				'note' => $note,
+				'date_actuelle' => $date_actuelle,
+			));
+
+			header("Location: ".$_SERVER['HTTP_REFERER']."");
+
+		?>
+	</body>
+</html>
--- www/ajoutCommentaireUser.php
+++ www/ajoutCommentaireUser.php
@@ -1,28 +1,46 @@
-<?php
-	
-	ini_set('display_errors', 'On');
-	$target_user_id = $_POST['target_user_id'];
-	$current_user_id = $_POST['current_user_id'];
-	$commentaire = $_POST['commentaire'];
-	$note = $_POST['note'];
-	$date_actuelle = date("Y-m-d H:i:s");
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>PopcornTeque</title>
+    </head>
 
-	echo "Sauvegarde du commentaire terminé";
+    <body>
+    	<header>
+            <a href="index.php">
+            	<img src="images/logo.png" height="120px">
+            </a>
+    	</header>
 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
 
-	$req = $bdd->prepare('INSERT INTO COMMENTAIRES_UT VALUES(NULL, :current_user_id, :target_user_id, :commentaire, :note, :date_actuelle)');
-	$req->execute(array(
-		'current_user_id' => $current_user_id,
-		'target_user_id' => $target_user_id,
-		'commentaire' => $commentaire,
-		'note' => $note,
-		'date_actuelle' => $date_actuelle
-	));
+		<?php
+			
+			ini_set('display_errors', 'On');
+			$target_user_id = $_POST['target_user_id'];
+			$current_user_id = $_POST['current_user_id'];
+			$commentaire = $_POST['commentaire'];
+			$note = $_POST['note'];
+			$date_actuelle = date("Y-m-d H:i:s");
 
-?>
+			echo "Sauvegarde du commentaire terminé";
+
+			try{
+				$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+			}
+			catch (Exception $e){
+				die('Erreur : ' . $e->getMessage());
+			}
+
+			$req = $bdd->prepare('INSERT INTO COMMENTAIRES_UT VALUES(NULL, :current_user_id, :target_user_id, :commentaire, :note, :date_actuelle)');
+			$req->execute(array(
+				'current_user_id' => $current_user_id,
+				'target_user_id' => $target_user_id,
+				'commentaire' => $commentaire,
+				'note' => $note,
+				'date_actuelle' => $date_actuelle
+			));
+
+		?>
+	</body>
+</html>
