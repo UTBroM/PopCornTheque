eefac5a7dc456a15abcd1aa14fae7bb10407301d
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 16:26:07 +0100

    oubli erreur dans l'URL

--- www/detailsFilm.php
+++ www/detailsFilm.php
@@ -36,7 +36,7 @@
 		echo "<h2>Utilisateurs qui disposent de ce film (cliquez pour faire une demande d'emprunt):</h2><ul>";
 		while($donnees = $req2->fetch()){
 
-			echo '<li><a href="demandeEmprunt?uti_id=', htmlspecialchars($donnees['UTI_ID']), '&sup_id=', $donnees['SUP_ID'], '">', htmlspecialchars($donnees['UTI_ID']), ' (', htmlspecialchars($donnees['SUP_NOM']), ')', '</a></li>';
+			echo '<li><a href="demandeEmprunt.php?uti_id=', htmlspecialchars($donnees['UTI_ID']), '&sup_id=', $donnees['SUP_ID'], '">', htmlspecialchars($donnees['UTI_ID']), ' (', htmlspecialchars($donnees['SUP_NOM']), ')', '</a></li>';
 
 		}
 		echo '</ul>';
