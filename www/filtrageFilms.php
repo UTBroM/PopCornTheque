35b92a26f0344e5eb7101df45aa6e031db332708
Victor SENE <victor@sene.ovh>
2015-01-06 13:57:21 +0100

    ajout des balises html

--- www/filtrageFilms.php
+++ www/filtrageFilms.php
@@ -1,28 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
+	<?php
 
-	ini_set('display_errors', 'On');
+		ini_set('display_errors', 'On');
 
-	$film = $_POST['film'];
+		$film = $_POST['film'];
 
-	echo "Recherche du film ", htmlspecialchars($film);
+		echo "Recherche du film ", htmlspecialchars($film);
 
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e)
-	{
-		die('Erreur : ' . $e->getMessage());
-	}
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		}
+		catch (Exception $e)
+		{
+			die('Erreur : ' . $e->getMessage());
+		}
 
-	$req = $bdd->prepare("SELECT *  FROM FILM WHERE FILM_TITRE LIKE CONCAT('%', ?, '%')");
-	$req->execute(array($film));
+		$req = $bdd->prepare("SELECT *  FROM FILM WHERE FILM_TITRE LIKE CONCAT('%', ?, '%')");
+		$req->execute(array($film));
 
-	while($donnees = $req->fetch()){
+		while($donnees = $req->fetch()){
 
-		echo '<br />', htmlspecialchars($donnees['FILM_TITRE']), '<br />';
-		echo '<img src="', $donnees['FILM_AFFICHE'], '"></br>';
+			echo '<br />', htmlspecialchars($donnees['FILM_TITRE']), '<br />';
+			echo '<img src="', $donnees['FILM_AFFICHE'], '"></br>';
 
-	}
+		}
 
-?>
+	?>
+	
+	</body>
+</html>
+
