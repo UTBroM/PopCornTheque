ca73044cc177d0815dc3a7ebdfb8ff53f123a2b9
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 18:25:50 +0100

    Suppression des entêtes HTML5
    Ça sert à rien

--- www/connexion.php
+++ www/connexion.php
@@ -1,50 +1,38 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style.css" />
-		<title>PopcornTeque</title>
-	</head>
+<?php
 
-	<body>
-		<?php
+	ini_set('display_errors', 'On');
 
-			ini_set('display_errors', 'On');
+	session_start();
+	$_SESSION = array();
 
-			session_start();
-			$_SESSION = array();
+	$user = $_POST['user'];
+	$password = sha1($_POST['password']);
 
-			$user = $_POST['user'];
-			$password = sha1($_POST['password']);
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
 
-			try
-			{
-				$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-			}
-			catch (Exception $e)
-			{
-				die('Erreur : ' . $e->getMessage());
-			}
+	$req = $bdd->prepare('SELECT UTI_MOT_DE_PASSE FROM UTILISATEURS WHERE UTI_ID = :nom');
+	$req->execute(array(
+		'nom' => $user
+	));
 
-			$req = $bdd->prepare('SELECT UTI_MOT_DE_PASSE FROM UTILISATEURS WHERE UTI_ID = :nom');
-			$req->execute(array(
-				'nom' => $user
-			));
+	$realpassword = $req->fetch()[0];
 
-			$realpassword = $req->fetch()[0];
+	if ($realpassword == $password){
 
-			if ($realpassword == $password){
-
-				echo "<p>Connexion réussie</p><p><a href=","index.php",">Revenir à la page d'accueil</a></p>";
-				$_SESSION['login'] = $user;
-				$_SESSION['password'] = $password;
+		$_SESSION['login'] = $user;
+		$_SESSION['password'] = $password;
 		header('Location: index.php');
 
-			}
-			else{
+	}
+	else{
 
-				echo '<p>Mauvais mot de passe</p>';
-			}
-		?>
-	</body>
-</html>
+		echo '<p>Mauvais mot de passe</p>';
+	}
+?>
