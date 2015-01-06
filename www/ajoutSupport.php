c44506e4c7d4baadf8b82576206c29eced1c1e08
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-06 09:38:00 +0100

    Conformité HTML5 ajoutSupport.php

--- www/ajoutSupport.php
+++ www/ajoutSupport.php
@@ -1,25 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
+
+	<?php
 	
-	ini_set('display_errors', 'On');
+		ini_set('display_errors', 'On');
 
-	$film_id = $_POST['film_id'];
-	$utilisateur_id = $_POST['utilisateur_id'];
-	$support_nom = $_POST['support_nom'];
+		$film_id = $_POST['film_id'];
+		$utilisateur_id = $_POST['utilisateur_id'];
+		$support_nom = $_POST['support_nom'];
 
-	echo "Support ajouté";
+		echo "Support ajouté";
 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-	}
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
+		try{
+			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+		}
+		catch (Exception $e){
+			die('Erreur : ' . $e->getMessage());
+		}
 
-	$req = $bdd->prepare('INSERT INTO SUPPORT VALUES(NULL, :film_id, :utilisateur_id, :support_nom, 1)');
-	$req->execute(array(
-		'film_id' => $film_id,
-		'utilisateur_id' => $utilisateur_id,
-		'support_nom' => $support_nom,
-	));
+		$req = $bdd->prepare('INSERT INTO SUPPORT VALUES(NULL, :film_id, :utilisateur_id, :support_nom, 1)');
+		$req->execute(array(
+			'film_id' => $film_id,
+			'utilisateur_id' => $utilisateur_id,
+			'support_nom' => $support_nom,
+		));
 
-?>
+	?>
+
+	</body>
+</html>
