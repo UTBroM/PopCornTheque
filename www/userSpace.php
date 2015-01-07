eaff27c60b135151898f275e76c409683225813b
Victor SENE <victor@sene.ovh>
2015-01-07 19:35:27 +0100

    déplacement boutonrendre pour plus de cohérence

--- www/userSpace.php
+++ www/userSpace.php
@@ -91,12 +91,12 @@
 			<p>
 				<h2>Mes emprunts<br/></h2>
 				<?php 
-					$req5 = $bdd->prepare("SELECT SUP_ID, EMPR_RETOUR_THEORIQUE, EMPR_ID FROM EMPRUNT WHERE UTI_ID = ? AND EMPR_RENDU = FALSE");
+					$req5 = $bdd->prepare("SELECT SUP_ID, EMPR_RETOUR_THEORIQUE FROM EMPRUNT WHERE UTI_ID = ? AND EMPR_RENDU = FALSE");
 					$req5->execute(array($user));
 
 					while($donnees5 = $req5->fetch()){
 						echo htmlspecialchars($donnees5['SUP_ID']), "   à rendre le  ", htmlspecialchars($donnees5['EMPR_RETOUR_THEORIQUE']), "     ";
-						echo '<a href="retour.php?emprunt_id=', htmlspecialchars($donnees5['EMPR_ID']), '">Rendre</a><br/>';
+						
 					}
 					$req5->closeCursor();
 				?>
@@ -107,11 +107,12 @@
 			<p>
 				<h2>Mes prets<br/></h2>
 				<?php 
-					$req5 = $bdd->prepare("SELECT S.UTI_ID, E.EMPR_RETOUR_THEORIQUE, F.FILM_TITRE, E.UTI_ID FROM EMPRUNT AS E INNER JOIN SUPPORT AS S ON E.SUP_ID = S.SUP_ID INNER JOIN FILM AS F ON S.FILM_ID = F.FILM_ID WHERE S.UTI_ID = ? ");
+					$req5 = $bdd->prepare("SELECT S.UTI_ID, E.EMPR_RETOUR_THEORIQUE, F.FILM_TITRE, E.UTI_ID, E.EMPR_ID FROM EMPRUNT AS E INNER JOIN SUPPORT AS S ON E.SUP_ID = S.SUP_ID INNER JOIN FILM AS F ON S.FILM_ID = F.FILM_ID WHERE S.UTI_ID = ? ");
 					$req5->execute(array($user));
 
 					while($donnees5 = $req5->fetch()){
-						echo htmlspecialchars($donnees5['FILM_TITRE']), "    a été emprunté par    ", htmlspecialchars($donnees5['UTI_ID']), "    et devra etre rendu le    ", htmlspecialchars($donnees5['EMPR_RETOUR_THEORIQUE']), "<br/>";
+						echo htmlspecialchars($donnees5['FILM_TITRE']), "    a été emprunté par    ", htmlspecialchars($donnees5['UTI_ID']), "    et devra etre rendu le    ", htmlspecialchars($donnees5['EMPR_RETOUR_THEORIQUE']);
+						echo '<a href="retour.php?emprunt_id=', htmlspecialchars($donnees5['EMPR_ID']), '">Rendre</a><br/>';
 					}
 					$req5->closeCursor();
 				?>
