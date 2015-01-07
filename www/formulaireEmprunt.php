86bb10a2fa629837720dadcff6a1a0c71a1105f3
Victor SENE <victor@sene.ovh>
2015-01-07 17:33:00 +0100

    nouveau formulaire emprunt

---
+++ www/formulaireEmprunt.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>PopCornTheque</title>
+	</head>
+	<body>
+		<?php 
+			$current_id_user = $_GET['current_id_user'];
+			$current_id_support = $_GET['current_id_support'];
+		?>
+
+		<form action="emprunt.php" method="post">
+			<p>
+				ID de l'emprunteur : <input type="hidden" name="current_id_user" required value="<?php echo htmlspecialchars($current_id_user);?>"/></br>
+				ID du support à emprunter:<input type="hidden" name="current_id_support" required value="<?php echo htmlspecialchars($current_id_support);?>"/></br>
+				Date de retour:<input type="date" name="retour_emprunt_date" required /></br>
+				
+				<input type="submit" value="Valider">
+			</p>
+
+		</form>
+
+	</body>
+</html>
