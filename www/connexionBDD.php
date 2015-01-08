517f0320c6bb2f5367a2e07f418624fe3b8e573f
Victor SENE <victor@sene.ovh>
2015-01-08 17:10:32 +0100

    rectification fichier connexion bdd

--- www/connexionBDD.php
+++ www/connexionBDD.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="style.css" />
-        <title>PopcornTeque</title>
-    </head>
+<?php	
 
-    <body>
-	<?php	
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
 
-		try
-		{
-			$bdd = new PDO('mysql:host=localhost;dbname=PopCornTheque', 'poppoppop', 'nnd47D2JQWAzh97H');
-		}
-		catch (Exception $e)
-		{
-			die('Erreur : ' . $e->getMessage());
-		}
-
-	?>
-	</body>
-</html>
+?>
