3543387611721953c9bf1a883708364c7a576977
Victor SENE <victor@sene.ovh>
2015-01-08 21:30:12 +0100

    symbole header

--- www/header.php
+++ www/header.php
@@ -16,9 +16,9 @@
 		}
 	?>
 		<li><a href="index.php">&#8962; Accueil</a></li>
-		<li><a href="userSpace.php">Mon Espace</a></li>
-		<li><a href="ajoutAmi.html">Ajouter un(e) ami(e)</a></li>
-		<li><a href="deconnexion.php">Deconnexion</a></li>
+		<li><a href="userSpace.php">&#9977; Mon Espace</a></li>
+		<li><a href="ajoutAmi.html">&#10084; Ajouter un(e) ami(e)</a></li>
+		<li><a href="deconnexion.php">&#11144; Deconnexion</a></li>
 	</ul>
 	
 </nav>
