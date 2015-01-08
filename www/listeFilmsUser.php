dad8f91c429a14139ae3d037207e285bb7aa9135
Victor SENE <victor@sene.ovh>
2015-01-08 10:14:55 +0100

    ajout header & on peut cliquer sur les films d'un user

--- www/listeFilmsUser.php
+++ www/listeFilmsUser.php
@@ -8,7 +8,8 @@
 
 	<body>
 	<?php
-		
+		include 'header.php'; 
+
 		ini_set('dispay_errors', 'On');
 
 		$utilisateur_id = $_POST['utilisateur_id'];
@@ -29,7 +30,7 @@
 		echo "<thead>\n<tr>\n<th>Id du film\n<th>Titre\n<th>Nom du support\n<th>\n";
 		echo "<tbody>\n";
 		while($donnees = $req->fetch()){
-			echo "<tr>\n<td>",$donnees['FILM_ID'],"\n<td>",$donnees['FILM_TITRE'],"\n<td>",htmlspecialchars($donnees['SUP_NOM']),"\n";
+			echo "<tr>\n<td>",$donnees['FILM_ID'],'\n<td><a href="detailsFilm.php?idfilm=', htmlspecialchars($donnees['FILM_ID']), '">', $donnees['FILM_TITRE'],"</a>\n<td>",htmlspecialchars($donnees['SUP_NOM']),"\n";
 			echo '<td><img src="', $donnees['FILM_AFFICHE'], '" width=40% height=40%>';
 		}
 		echo"</table>";
