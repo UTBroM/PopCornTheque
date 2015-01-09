c53d00f3d4028e675a957cde50a739ecf928e6d1
Victor SENE <victor@sene.ovh>
2015-01-09 20:52:45 +0100

    suppr lien dans popup

--- www/header.php
+++ www/header.php
@@ -4,7 +4,7 @@
 		<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
 			<img alt="Licence Creative Commons" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
 		</a>
-		<span>Cette œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a></span>
+		<span>Cette œuvre est mise à disposition selon les termes de la Licence Creative Commons Attribution 4.0 International</span>
 	</div>
 	
 
