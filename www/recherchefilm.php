ef4d1d9a0a9bac6887d44360404272caa2ddbdaa
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-09 20:49:47 +0100

    Modif requete api

--- www/recherchefilm.php
+++ www/recherchefilm.php
@@ -19,24 +19,20 @@
 
 			$filmrecherche = urlencode($_POST['Film']);
 
-			$object = json_decode(file_get_contents("http://www.omdbapi.com/?s=$filmrecherche"));
+			$object = json_decode(file_get_contents("http://www.omdbapi.com/?s=$filmrecherche&type=movie"));
 
 			foreach($object->Search as $Film){
 
-				if($Film->Type == "movie"){
+				$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
 
-					$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
+				echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
+				$posterURL = $details->Poster;
 
-					echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
-					$posterURL = $details->Poster;
+				if ($posterURL != "N/A"){
 
-					if ($posterURL != "N/A"){
-
-						$posterFile = basename($posterURL);
-						if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
-						echo '<img src="', "tempPoster/$posterFile", '"></br>';
-
-					}
+					$posterFile = basename($posterURL);
+					if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
+					echo '<img src="', "tempPoster/$posterFile", '"></br>';
 
 				}
 
