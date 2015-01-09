c3bb0ae0cfa0d427bebb536ad98b6dcef3f95258
Victor SENE <victor@sene.ovh>
2015-01-09 18:11:02 +0100

    suppression footer et réarangement index

--- www/index.php
+++ www/index.php
@@ -21,32 +21,27 @@
 			</form>
 		</section>
 		
-	<ul class="tableau-film">
-	
-	<?php
+		<ul class="tableau-film">
+			<?php
+				include 'connexionBDD.php';
 
-		include 'connexionBDD.php';
+				$req = $bdd->prepare("SELECT *  FROM FILM");
+				$req->execute(array($film));
 
-		$req = $bdd->prepare("SELECT *  FROM FILM");
-		$req->execute(array($film));
+				while($donnees = $req->fetch()){
 
-		while($donnees = $req->fetch()){
-
-			echo '<a href="detailsFilm.php?idfilm=',$donnees['FILM_ID'],'"><li>';
-			echo '<h4>',htmlspecialchars($donnees['FILM_TITRE']),"</h4>\n";
-			echo '<img src="', $donnees['FILM_AFFICHE'], '">';
-			echo "\n</li></a>";
-
-		}
-
-	?>
-
-	</ul>
+					echo '<a href="detailsFilm.php?idfilm=',$donnees['FILM_ID'],'"><li>';
+					echo '<h4>',htmlspecialchars($donnees['FILM_TITRE']),"</h4>\n";
+					echo '<img src="', $donnees['FILM_AFFICHE'], '">';
+					echo "\n</li></a>";
+				}
+			?>
+		</ul>
 
 
-	<a href="rechercheFilm.html" title="Ajouter film possédé" style="position: fixed; bottom: 0; right: 0;"><div id="badge-plus"><img src="images/plus.png" alt="Ajouter un film possédé"></div></a>
-
-	<footer><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a></footer>
+		<a href="rechercheFilm.html" title="Ajouter film possédé" style="position: fixed; bottom: 0; right: 0;">
+			<div id="badge-plus"><img src="images/plus.png" alt="Ajouter un film possédé"></div>
+		</a>
 
 	</body>
 </html>
