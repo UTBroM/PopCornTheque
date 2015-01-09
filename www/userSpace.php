b01caa02772d7c401de9a9b420028b29fdb47522
Victor SENE <victor@sene.ovh>
2015-01-09 18:39:00 +0100

    order by pour films

--- www/userSpace.php
+++ www/userSpace.php
@@ -19,7 +19,7 @@
 
 			include 'connexionBDD.php';
 
-			$req = $bdd->prepare("SELECT F.FILM_ID, SUP_NOM, FILM_AFFICHE, FILM_TITRE FROM SUPPORT AS S INNER JOIN FILM AS F ON F.FILM_ID = S.FILM_ID WHERE UTI_ID = ?");
+			$req = $bdd->prepare("SELECT F.FILM_ID, SUP_NOM, FILM_AFFICHE, FILM_TITRE FROM SUPPORT AS S INNER JOIN FILM AS F ON F.FILM_ID = S.FILM_ID WHERE UTI_ID = ? ORDER BY F.FILM_TITRE");
 			$req->execute(array($user));
 
 			echo "<br/><br/><table>\n<caption><h2>Mes Films</h2></caption>\n";
@@ -90,11 +90,14 @@
 			<p>
 				<h2>Mes emprunts<br/></h2>
 				<?php 
-					$req5 = $bdd->prepare("SELECT SUP_ID, EMPR_RETOUR_THEORIQUE FROM EMPRUNT WHERE UTI_ID = ? AND EMPR_RENDU = FALSE");
+					$req5 = $bdd->prepare("SELECT E.EMPR_RETOUR_THEORIQUE, F.FILM_TITRE FROM EMPRUNT AS E INNER JOIN SUPPORT AS S ON E.SUP_ID = S.SUP_ID INNER JOIN FILM AS F ON S.FILM_ID = F.FILM_ID WHERE UTI_ID = ? AND EMPR_RENDU = FALSE");
 					$req5->execute(array($user));
 
+					echo "<br/><br/><table>\n<caption><h2>Mes emprunts</h2></caption>\n";
+					echo "<thead>\n<tr>\n<th>Titre\n<th>Nom du support\n<th>\n";
+					echo "<tbody>\n";
 					while($donnees5 = $req5->fetch()){
-						echo htmlspecialchars($donnees5['SUP_ID']), "   à rendre le  ", date("d/m/Y", strtotime($donnees5['EMPR_RETOUR_THEORIQUE'])), "     ";
+						echo htmlspecialchars($donnees5['FILM_TITRE']), "   à rendre le  ", date("d/m/Y", strtotime($donnees5['EMPR_RETOUR_THEORIQUE'])), "     ";
 						
 					}
 					$req5->closeCursor();
