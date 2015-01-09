c1a42ad2e2f703c30fa0759869fa0f54d81e292b
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-09 13:48:11 +0100

    Mauvaises tabulations

--- www/index.php
+++ www/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="style.css" />
-        <title>PopcornTeque</title>
-    </head>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
 
-    <body>
+	<body>
 
 		<?php include 'header.php'; ?>
 
@@ -44,9 +44,9 @@
 	</ul>
 
 
-        <a href="rechercheFilm.html" title="Ajouter film possédé" style="position: fixed; bottom: 0; right: 0;"><div id="badge-plus"><img src="images/plus.png" alt="Ajouter un film possédé"></div></a>
+	<a href="rechercheFilm.html" title="Ajouter film possédé" style="position: fixed; bottom: 0; right: 0;"><div id="badge-plus"><img src="images/plus.png" alt="Ajouter un film possédé"></div></a>
 
 	<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a>.
 
-    </body>
+	</body>
 </html>
