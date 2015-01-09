4fbc7450997af0408709ab92fbf9808a766ed96d
Victor SENE <victor@sene.ovh>
2015-01-09 20:34:37 +0100

    déplacement span pour popup

--- www/header.php
+++ www/header.php
@@ -4,8 +4,9 @@
 		<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
 			<img alt="Licence Creative Commons" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
 		</a>
+		<span>Cette œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a></span>
 	</div>
-	<span>Cette œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a></span>
+	
 
 </header>
 <nav>
