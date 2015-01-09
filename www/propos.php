1c382c3f1d7fec73a8365853f2169c55937aa1c2
Victor SENE <victor@sene.ovh>
2015-01-09 20:30:59 +0100

    remplissage a propos

--- www/propos.php
+++ www/propos.php
@@ -8,6 +8,8 @@
 
 	<body>
 		<?php include 'header.php'; ?>
-		<h1>héhéh</h1>
+		<h1> Vous avez aimé le site ?</h1>
+		<p>N'oublions pas de remercier les développeur de l'API d'IMDb (<a href="http://www.omdbapi.com/">https://www.youtube.com/watch?v=gy1B3agGNxw</a>) qui nous a permis de remplir notre base de donnée de superbe affiches !<br/>
+			Ce site est sous <a href="http://creativecommons.org/licenses/by/4.0/">license creative common</a>. Merci d'en tenir compte et de respecter ce choix. 
 	</body>
 </html>
