4872eaec73376886fa59399b29c9e0a6e836a61d
Victor SENE <victor@sene.ovh>
2015-01-09 19:03:41 +0100

    div pour license

--- www/header.php
+++ www/header.php
@@ -1,8 +1,10 @@
 <header>
 	<a href="index.php"><img src="images/logo.png" height="140px"></a>
-	<a rel="license" class="license" href="http://creativecommons.org/licenses/by/4.0/">
-		<img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
-	</a>
+	<div class="license">
+		<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+			<img alt="Licence Creative Commons" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+		</a>
+	</div>
 	<span>Cette œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Licence Creative Commons Attribution 4.0 International</a></span>
 
 </header>
