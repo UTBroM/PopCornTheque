d259b534f0d28f48389c02271816dc438470fb2a
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-01-09 20:55:24 +0100

    Lien plus grands lors d'une recherche

--- www/recherchefilm.php
+++ www/recherchefilm.php
@@ -25,16 +25,18 @@
 
 				$details = json_decode(file_get_contents("http://www.omdbapi.com/?i=$Film->imdbID&plot=short&r=json"));
 
-				echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '</a></br>';
+				echo '<a href=', "enregistrerfilm.php?detailsfilm=$Film->imdbID", '>', $Film->Title, '<br />';
 				$posterURL = $details->Poster;
 
 				if ($posterURL != "N/A"){
 
 					$posterFile = basename($posterURL);
 					if(!file_exists("tempPoster/$posterFile")) file_put_contents("tempPoster/$posterFile", file_get_contents("$posterURL"));
-					echo '<img src="', "tempPoster/$posterFile", '"></br>';
+					echo '<img src="', "tempPoster/$posterFile", '"><br />';
 
 				}
+
+				echo '</a>';
 
 			}
 
