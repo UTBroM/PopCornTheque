1dcb0ec2f3d314a8e79615d66b94d541fd6ad21a
Victor SENE <victor@sene.ovh>
2015-01-09 20:52:31 +0100

    erreur orthographes

--- www/propos.php
+++ www/propos.php
@@ -12,8 +12,8 @@
 		<p>N'oublions pas de remercier les développeur de l'API d'OMDb (<a href="http://www.omdbapi.com/">http://www.omdbapi.com/</a>) qui nous a permis de remplir notre base de donnée de superbe affiches !<br/>
 			Ce site est sous <a href="http://creativecommons.org/licenses/by/4.0/">license creative common</a>. Merci d'en tenir compte et de respecter ce choix. <br/><br/>
 			
-			Nous avions pour but de créer une application qui va permettre à chacun de gérer sa vidéothèque personnelle et permettre l'échange de différent support entre utilisateurs. Une fonction social est maintenant intégré à l'application avec une liste d'amis pour chaque utilisateurs et la possibilité de créer une liste de film à voir.<br/>
-			Ce projet entre dans le cadre d'un UV dispensée à l'<a href="utbm.fr">UTBM</a> et fût réalisé par Florian STAINE, William OBRY, Paul TREHIOU et <a href="sene.ovh">Victor SENE</a>.<br/>
-			
+			Nous avions pour but de créer une application qui va permettre à chacun de gérer sa vidéothèque personnelle et permettre l'échange de différent support entre utilisateurs. Une fonction sociale est maintenant intégrée à l'application avec une liste d'amis pour chaque utilisateurs et la possibilité de créer une liste de film à voir.<br/>
+			Ce projet entre dans le cadre de l'UV SI20 dispensée à l'<a href="utbm.fr">UTBM</a> et fût réalisé par Florian STAINE, William OBRY, Paul TREHIOU et <a href="sene.ovh">Victor SENE</a>.<br/>
+
 	</body>
 </html>
