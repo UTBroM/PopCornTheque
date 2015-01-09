b56af2b3a204b50951beace499b0e790f4ebbf11
Victor SENE <victor@sene.ovh>
2015-01-09 19:50:06 +0100

    création A propos

---
+++ www/propos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>PopcornTeque</title>
+	</head>
+
+	<body>
+		<?php include 'header.php'; ?>
+		<h1>héhéh</h1>
+	</body>
+</html>
